02e83e97fb2287905ff0947ddfc3fad86fd115e6
Harshal <harshalpatilself@gmail.com>
2023-03-28 21:12:45 +0530

    nf: Insta_Reel_Code.c

---
+++ Insta_Reel_Code.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+#include<conio.h>
+#include<windows.h>
+int main()
+{
+    char a[]=" HELLO MY NAME IS HARSHAL \n This is My Official Account \n Here I will be Posting Content Related to \n Coding, Photography and More such Things \n Do Follow and \n Don't forget to Like \n THANK YOU ";
+    int i,n;
+
+    n=strlen(a);
+    
+    for(i=0;i<n;i++)
+    {
+        Sleep(100);
+        printf("%c",a[i]);
+    }
+    
+    return 0;
+}
+
